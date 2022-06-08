b27fce20a842238bd5bbfb72fc3ed5f6473c2b42
Vikas Yadav <web.yvikas@gmail.com>
2022-06-08 20:19:03 +0530

    Changing the hero Section with preffered role

--- src/Hero.js
+++ src/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
         <img src="/images/Programming-amico.png" className='w-56 mt-20 xl:w-96 xl:ml-40' alt="" />
         <div className='text-white flex items-center flex-col xl:block  xl:ml-20 '>
             <p className='text-[25px] heroText ' >my name is <span className='text-[#EC0B0B]'>Vikas</span> i am a </p>
-            <h1 className='text-[40px] my-10 md:text-[80px] heroHeader text-[#EC0B0B]'>Web Developer</h1>
+            <h1 className='text-[40px] my-10 md:text-[80px] heroHeader text-[#EC0B0B]'>React Js Developer</h1>
             <h2 className=' heroText text-[20px] xl:text-[25px]'>Living In <span className='text-[#EC0B0B]'>Chandigarh</span>,  India </h2>
             <Link smooth={true} to={'Portfolio'}> <button className='bg-[#EC0B0B] mt-40 md:mt-10 animate-bounce md:animate-none w-40 my-6 h-10  outline-white rounded transition hover:outline'>Projects</button></Link>
         </div>

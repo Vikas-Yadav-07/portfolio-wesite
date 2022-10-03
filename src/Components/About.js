3f4c2efa00ccdd794e252fe46bc652b355c1f936
Vikas Yadav <102656687+Vikas-Yadav-07@users.noreply.github.com>
2022-10-03 23:57:48 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,14 +8,14 @@ function About() {
         <div className='flex justify-center text-center items-center flex-col w-full h-full'>
             <h1 className='text-4xl  logo border-b shadow-inherit  text-[#EC0B0B]  '>About Me</h1>
             <Slide direction='up'>
-            <p className='text-center p-4  shadow-2xl font-bold text-xl'>I Am A Pationate Entry Level Front-End / React Js Developer
-                With A keen Interest In Programming 
+            <p className='text-center p-4  shadow-2xl font-bold text-xl'>Expirienced Frontend Engineer with ability to Undertake Any project from scratch to production Build 
             </p>
             </Slide>
 
             <Slide delay={200} direction='up'>
-            <p className='text-center shadow-2xl p-4 font-bold text-xl'>I Enjoy Coding Like I am playing A Video Game. I am a Fast Learner and I always be ready to face 
-                New Challenges
+            <p className='text-center shadow-2xl p-4 font-bold text-xl'>
+              Have ability to grasp new technologies in very short span of time ,Have a top notch probnlem solving attitude to perform in complex situations, I Have developed varios application using React, Redux , Electron js , React native ,
+                Mui , tailwind css , bootstrap , REST API , node/express js.
             </p>
             </Slide>
         
